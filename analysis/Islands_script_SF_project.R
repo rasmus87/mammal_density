e2d00d9d886698697b64f6dd4cda8afb6fbf51f0
Rasmus <rasmus.iwfly@gmail.com>
2020-05-07 07:51:44 +0200

    Check island script

--- analysis/Islands_script_SF_project.R
+++ analysis/Islands_script_SF_project.R
@@ -22,16 +22,19 @@ write_tsv(islands, "output/Islands.txt")
 
 
 dens.post <- read_csv("builds/333_densities_post.pred.10k.sample.csv")
+# We shuold switch to this in next update
+#dens.post <- read_csv("builds/3_densities_post.pred.csv")
 
 # small.pop.sp <- "Prionailurus_bengalensis"
 # island.Iriomote <- 290.68 # km2
 # dens %>% filter(Binomial.1.2 == small.pop.sp) %>% select(log10.density.median, log10.lower.95hpd, log10.upper.95hpd) %>% 10^.*290.68
 
+
+# Build likelyhood for 500 animals
 islands <- read_tsv("data/Islands.txt")
 
 islands$Species_Name[-(1:2)][!islands$Species_Name[-(1:2)] %in% names(dens.post)]
 
-i = 19 # Dingo
 for (i in 3:132) {
   # home <- which(islands[i, 7:184] == TRUE)
   home <- (7:184)-6 # Fix calc for all islands
@@ -45,14 +48,11 @@ for (i in 3:132) {
     islands[i, j + 6] <- result
   }
 }
-
 write_tsv(islands, "output/Islands_500_likelihood_all_islands.txt")
 
 
-
+# Build likelyhood for 1000 animals
 islands <- read_tsv("data/Islands.txt")
-
-islands$Species_Name[-(1:2)][!islands$Species_Name[-(1:2)] %in% names(dens.post)]
 
 for(i in 3:132) {
   # home <- which(islands[i, 7:184] == TRUE)
@@ -67,5 +67,4 @@ for(i in 3:132) {
     islands[i, j + 6] <- result
   }
 }
-
 write_tsv(islands, "output/Islands_1000_likelihood_all_islands.txt")
