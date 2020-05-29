029ad97290ddcf5362d92ab93c5ce426eea7377d
Rasmus <rasmus.iwfly@gmail.com>
2020-05-29 17:51:24 +0200

    Update islands script and output new values

--- analysis/Islands_script_SF_project.R
+++ analysis/Islands_script_SF_project.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-dens <- read_csv("builds/imputed.density_3.csv")
+dens <- read_csv("output/Table S4 Imputed density.csv")
 
 islands <- read_tsv("data/Islands.txt")
 
@@ -21,50 +21,57 @@ islands <- islands %>% left_join(dens)
 write_tsv(islands, "output/Islands.txt")
 
 
-dens.post <- read_csv("builds/333_densities_post.pred.10k.sample.csv")
+# dens.post <- read_csv("builds/333_densities_post.pred.10k.sample.csv")
 # We shuold switch to this in next update
-#dens.post <- read_csv("builds/3_densities_post.pred.csv")
-
-# small.pop.sp <- "Prionailurus_bengalensis"
-# island.Iriomote <- 290.68 # km2
-# dens %>% filter(Binomial.1.2 == small.pop.sp) %>% select(log10.density.median, log10.lower.95hpd, log10.upper.95hpd) %>% 10^.*290.68
-
+dens.post <- read_csv("builds/3_densities_post.pred.csv")
 
 # Build likelyhood for 500 animals
 islands <- read_tsv("data/Islands.txt")
 
+# Lonta felina not included in out dataset
 islands$Species_Name[-(1:2)][!islands$Species_Name[-(1:2)] %in% names(dens.post)]
 
-for (i in 3:132) {
-  # home <- which(islands[i, 7:184] == TRUE)
-  home <- (7:184)-6 # Fix calc for all islands
-  j <- home[1]
-  for(j in home) {
-    island.size <- islands[[2, j + 6]] 
+# Backup clean version
+islands.bak <- islands
+
+# Species numbers:
+# 3:132, 1:2 are extra data
+species.index <- 3:132
+# Island numbers:
+# 7:184, 1:6 are extra data
+island.index <- 7:184
+
+
+for (i in species.index) {
+  for(j in island.index) {
+    island.size <- islands[[2, j]] 
     species <- islands$Species_Name[i]
-    if(species == "Lontra_felina") next
+    if(species == "Lontra_felina") {
+      islands[i, j] <- NA
+      next
+    } 
     pop.size <- island.size * 10^dens.post[species]
     result <- mean(pop.size >= 500)
-    islands[i, j + 6] <- result
+    islands[i, j] <- result
   }
 }
 write_tsv(islands, "output/Islands_500_likelihood_all_islands.txt")
 
 
 # Build likelyhood for 1000 animals
-islands <- read_tsv("data/Islands.txt")
+islands <- islands.bak
 
-for(i in 3:132) {
-  # home <- which(islands[i, 7:184] == TRUE)
-  home <- (7:184)-6 # Fix calc for all islands
-  j <- home[1]
-  for(j in home) {
-    island.size <- islands[[2, j + 6]] 
+for (i in species.index) {
+  for(j in island.index) {
+    island.size <- islands[[2, j]] 
     species <- islands$Species_Name[i]
-    if(species == "Lontra_felina") next
+    if(species == "Lontra_felina") {
+      islands[i, j] <- NA
+      next
+    } 
     pop.size <- island.size * 10^dens.post[species]
     result <- mean(pop.size >= 1000)
-    islands[i, j + 6] <- result
+    islands[i, j] <- result
   }
 }
 write_tsv(islands, "output/Islands_1000_likelihood_all_islands.txt")
