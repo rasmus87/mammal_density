9b43b4704354ec93cc9cdd0b9c2915afa164712a
rasmus87 <rasmus.iwfly@gmail.com>
2021-07-30 12:34:44 +0200

    Reduce forest to terrestrial species

--- scripts/0.1_load_trees.R
+++ scripts/0.1_load_trees.R
@@ -8,5 +8,29 @@ library(ape)
 # Load PHYLACINE 1.2.1 posterior phylogeny distribution
 forest <- read.nexus("../PHYLACINE_1.2/Data/Phylogenies/Complete_phylogeny.nex")
 
+# Non terrestrials and humans
+bat.order <- "Chiroptera"
+sea.cow.order <- "Sirenia"
+whale.families <- c("Balaenidae", "Balaenopteridae", "Ziphiidae", 
+                    "Neobalaenidae", "Delphinidae", "Monodontidae", 
+                    "Eschrichtiidae", "Iniidae", "Physeteridae", 
+                    "Phocoenidae", "Platanistidae")
+seal.families <- c("Otariidae", "Phocidae", "Odobenidae")
+marine.carnivores <- c("Enhydra_lutris", "Lontra_felina", "Ursus_maritimus")
+humans <- "Homo"
+
+terrestrial <- mam %>% 
+  filter(!Order.1.2 %in% c(bat.order, sea.cow.order),
+         !Family.1.2 %in% c(whale.families, seal.families),
+         !Binomial.1.2 %in% marine.carnivores,
+         !Genus.1.2 %in% humans) %>% 
+  pull(Binomial.1.2)
+
+
+# Reduce forest to the designated species list
+species <- forest[[1]]$tip.label
+drop.species <- species[!species %in% terrestrial]
+forest <- lapply(forest, drop.tip, tip = drop.species)
+
 # Store for fast loading
 write_rds(forest, "builds/forest.rds")
