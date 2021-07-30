f05762046f99625f5166f845c6b13092c93f83e9
rasmus87 <rasmus.iwfly@gmail.com>
2021-07-30 16:16:41 +0200

    Update diagnostics script

--- scripts/2.1 mcmc.diagnostics.R
+++ scripts/2.1 mcmc.diagnostics.R
@@ -1,17 +1,20 @@
+# GLMM model diagnostics
+# 30/07-2021 Rasmus Ø Pedersen
+
+# Load libraries
 library(tidyverse)
 library(gridExtra)
 library(coda)
 library(MCMCglmm)
 library(ggpmisc)
 
-i = 1
-chain.1 <- readRDS(paste0("builds/mcmcglmms/tree", i, ".chain1.rds"))
-chain.2 <- readRDS(paste0("builds/mcmcglmms/tree", i, ".chain2.rds"))
-chain.3 <- readRDS(paste0("builds/mcmcglmms/tree", i, ".chain3.rds"))
-# 
-# chain.1 <- readRDS(paste0("builds/mcmcglmms/tree", i, ".chain1.alt.rds"))
-# chain.2 <- readRDS(paste0("builds/mcmcglmms/tree", i, ".chain2.alt.rds"))
-# chain.3 <- readRDS(paste0("builds/mcmcglmms/tree", i, ".chain3.alt.rds"))
+chain.1 <- readRDS(paste0("builds/mcmcglmms/chain1.rds"))
+chain.2 <- readRDS(paste0("builds/mcmcglmms/chain2.rds"))
+chain.3 <- readRDS(paste0("builds/mcmcglmms/chain3.rds"))
+ 
+# chain.1 <- readRDS(paste0("builds/mcmcglmms/chain1.alt.rds"))
+# chain.2 <- readRDS(paste0("builds/mcmcglmms/chain2.alt.rds"))
+# chain.3 <- readRDS(paste0("builds/mcmcglmms/chain3.alt.rds"))
 
 ### Checking 3 chains for tree 1
 sol <- bind_rows(as.data.frame(chain.1$Sol[, 1:2]), 
@@ -100,56 +103,3 @@ median(lambda)
 
 posterior.mode(lambda)
 HPDinterval(lambda)
-
-# unused:
-# summary(chain.1)
-# 
-# HPDinterval(chain.1$Sol[,1:2])
-# test0 <- predict.MCMCglmm(chain.1, df, interval = "confidence")[1:n.mam, ]
-# test1 <- predict.MCMCglmm(chain.1, df, interval = "confidence", marginal = NULL)[1:n.mam, ]
-# 
-# se.0 <- (test1[,3] - test1[,2])/2/qnorm(0.975)
-# 
-# post.pred <- MCMC.predict(chain.1, df)
-# post.pred.t <- t(post.pred)
-# post.pred.t <- post.pred.t[1:n.mam, ]
-# se.1 <- apply(post.pred.t, 1, sd)
-# hist(se.1)
-# 
-# se.test <- tibble(se = c(se.0, se.1), group = gl(2, length(se.0), labels = c("se.0", "se.1")))
-# ggplot(se.test, aes(se, col = group)) +
-#   geom_density() +
-#   geom_rug()
-# 
-# a <- as_tibble(t(post.pred.t))
-# colnames(a) <- mam$Binomial.1.2
-# a <- a %>% gather(key = "Species", value = "log10Density")
-# 
-# ggplot(a %>% filter(Species %in% sample(a$Species, 100)), 
-#        aes(x = log10Density, col = Species)) +
-#   geom_density() +
-#   scale_color_discrete(guide=FALSE)
-# 
-# coda::HPDinterval(post.pred.t[1,])
-# manual code: https://rdrr.io/cran/coda/src/R/HPDinterval.R
-# 
-# test1[1,]
-# a <- ecdf(post.pred.t[1,])
-# quantile(a, c(.5, .025, .975))
-# 
-# prob = .95
-# obj <- as.matrix(post.pred.t[1,])
-# vals <- apply(obj, 2, sort)
-# nsamp <- nrow(vals)
-# gap <- max(1, min(nsamp - 1, round(nsamp * prob)))
-# init <- 1:(nsamp - gap)
-# inds <-  which.min(vals[init + gap, ] - vals[init, ])
-# ans <- cbind(vals[inds],
-#              vals[inds + gap])
-# dimnames(ans) <- list(colnames(obj), c("lower", "upper"))
-# attr(ans, "Probability") <- gap/nsamp
-# ans
-# 
-# plot(pantheria$log10density ~ pantheria$log10BM, col = "red")
-# points(test0[,1] ~ mam$log10BM, col = "blue")
-# points(test1[,1] ~ mam$log10BM, col = "purple")
