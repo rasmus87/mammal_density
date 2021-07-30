198381098964a26d0506ac254aa69d2e26e3a08c
rasmus87 <rasmus.iwfly@gmail.com>
2021-07-30 11:43:02 +0200

    Update load data

--- scripts/0.2_load.data.R
+++ scripts/0.2_load.data.R
@@ -1,16 +1,19 @@
+# Load mammal density data
+# 30/07-2021 Rasmus Ø Pedersen
+
+# Load libraries
 library(tidyverse)
 library(ape)
 
-pantheria <- read_tsv("data/PanTHERIA_1-0_WR05_Aug2008.txt") %>% na_if(-999)
 
-names(pantheria) <- make.names(names(pantheria))
-pantheria <- pantheria %>% 
-  filter(!is.na(X21.1_PopulationDensity_n.km2)) %>% 
-  transmute(Binomial.Pantheria = paste(MSW05_Genus, MSW05_Species, sep = "_"),
-            density = X21.1_PopulationDensity_n.km2,
-            source = "PanTHERIA.2008")
-# Load taxonomy solver
-syn <- read_csv("../PHYLACINE_1.2/Data/Taxonomy/Synonymy_table_with_unaccepted_species.csv", col_types = cols(), guess_max = 5000)
+# Make taxonomy solver ----------------------------------------------------
+
+# Load PHYLACINE 1.2.1 trait data
+mam <- read_csv("../PHYLACINE_1.2/Data/Traits/Trait_data.csv", col_types = cols())
+
+# Load PHYLACINE 1.2.1 large synonomy
+syn <- read_csv("../PHYLACINE_1.2/Data/Taxonomy/Synonymy_table_with_unaccepted_species.csv")
+# Simplify syn table
 syn <- syn %>% 
   transmute(Binomial.1.2,
             Binomial.EltonTraits.1.0 = paste(EltonTraits.1.0.Genus, EltonTraits.1.0.Species, sep = "_"),
@@ -18,15 +21,28 @@ syn <- syn %>%
             Binomial.1.1 = paste(Genus.1.1, Species.1.0, sep = "_")) %>% 
   filter(!str_detect(Binomial.1.2, "000"))
 
-mam <- read_csv("../PHYLACINE_1.2/Data/Traits/Trait_data.csv", col_types = cols())
-
+# Make full synonomy matching table
 syn <- syn %>% 
-  gather(System, Binomial, -Binomial.1.2) %>% 
+  pivot_longer(names_to = "System", values_to = "Binomial", -Binomial.1.2) %>% 
   filter(Binomial.1.2 != Binomial) %>% 
   filter(!duplicated(Binomial)) %>% 
   bind_rows(mam %>% transmute(Binomial.1.2,
                               System = "Binomial.1.2",
                               Binomial = Binomial.1.2))
+
+
+
+# Load and align PanTHERIA ------------------------------------------------
+
+# Load PanTHERIA
+pantheria <- read_tsv("data/PanTHERIA_1-0_WR05_Aug2008.txt") %>% 
+  na_if(-999) %>% 
+  filter(!is.na(`21-1_PopulationDensity_n/km2`)) %>% 
+  transmute(Binomial.Pantheria = paste(MSW05_Genus, MSW05_Species, sep = "_"),
+            density = `21-1_PopulationDensity_n/km2`,
+            source = "PanTHERIA.2008")
+
+# Check for any mismatches in synonomy
 anti_join(pantheria, syn, by = c("Binomial.Pantheria" = "Binomial"))
 
 # Cercopithecus_pogonias
@@ -36,16 +52,23 @@ anti_join(pantheria, syn, by = c("Binomial.Pantheria" = "Binomial"))
 # Cebus_olivaceus is split into two species:
 # Cebus brunneus and Cebus kaapori - pick a random
 
+# Expand synonomy table to include these
 extra <- tibble(Binomial.1.2 = c("Cercopithecus_denti", "Cebus_brunneus"), 
-                System = "Personal.judgement", 
+                System = "Author.judgement", 
                 Binomial = c("Cercopithecus_pogonias", "Cebus_olivaceus"))
 syn <- syn %>% bind_rows(extra)
+
+# Check that we are fully aligned
 stopifnot(!anti_join(pantheria, syn, by = c("Binomial.Pantheria" = "Binomial")) %>% nrow)
 
-pantheria <- left_join(pantheria, syn %>% select(-System), by = c("Binomial.Pantheria" = "Binomial"))
+# Join synonomy and PHYLACINE traits to PanTHERIA
+pantheria <- pantheria %>% 
+  left_join(syn %>% 
+              select(-System), 
+            by = c("Binomial.Pantheria" = "Binomial")) %>% 
+  left_join(mam, by = "Binomial.1.2")
 
-pantheria <- pantheria %>% left_join(mam)
-
+# Which species are duplicates?
 dup <- pantheria %>% filter(duplicated(Binomial.1.2)) %>% pull(Binomial.1.2)
 pantheria %>% filter(Binomial.1.2 %in% dup) %>% select(Binomial.1.2, Binomial.Pantheria)
 
@@ -55,13 +78,16 @@ pantheria <- pantheria %>%
                                     "Felis_catus",
                                     "Alcelaphus_caama",
                                     "Alcelaphus_lichtensteinii"))
+# Make sure we don't have any further duplicates
 stopifnot(!anyDuplicated(pantheria$Binomial.1.2))
 
+# Reduce dataset columns
 pantheria <- pantheria %>% 
   mutate(log10density = log10(density),
          log10BM = log10(Mass.g)) %>% 
   select(Binomial.1.2, Order.1.2, Family.1.2, log10density, log10BM, source)
 
+# Non terrestrials and humans
 bat.order <- "Chiroptera"
 sea.cow.order <- "Sirenia"
 whale.families <- c("Balaenidae", "Balaenopteridae", "Ziphiidae", 
@@ -70,31 +96,44 @@ whale.families <- c("Balaenidae", "Balaenopteridae", "Ziphiidae",
                     "Phocoenidae", "Platanistidae")
 seal.families <- c("Otariidae", "Phocidae", "Odobenidae")
 marine.carnivores <- c("Enhydra_lutris", "Lontra_felina", "Ursus_maritimus")
+humans <- "Homo"
 
-terrestrial <- mam %>% filter(!Order.1.2 %in% c(bat.order, sea.cow.order),
-                              !Family.1.2 %in% c(whale.families, seal.families),
-                              !Binomial.1.2 %in% marine.carnivores) %>% pull(Binomial.1.2)
+terrestrial <- mam %>% 
+  filter(!Order.1.2 %in% c(bat.order, sea.cow.order),
+         !Family.1.2 %in% c(whale.families, seal.families),
+         !Binomial.1.2 %in% marine.carnivores,
+         !Genus.1.2 %in% humans) %>% 
+  pull(Binomial.1.2)
 
+# Filter to terrestrial species only
 pantheria <- pantheria %>% 
   filter(Binomial.1.2 %in% terrestrial) %>% 
   select(-source) %>% 
   mutate(dataset = "density")
+
+# Save dataset for imputation
 write_csv(pantheria, "builds/imputation_dataset.csv")
 
+# Turn pantheria into data.frame for MCMCglmm
 pantheria <- as.data.frame(pantheria)
 
+# Prepare mam for imputation
 mam <- mam %>% 
   filter(Binomial.1.2 %in% terrestrial) %>% 
-  mutate(log10BM = log10(Mass.g), log10density = NA)
-mam <- mam %>% mutate(dataset = "mam")
-mam <- mam %>% select(names(pantheria))
-mam <- as.data.frame(mam) # Very important for MCMCglmm
+  mutate(log10BM = log10(Mass.g), 
+         log10density = NA,
+         dataset = "mam") %>% 
+  select(names(pantheria))
+# Turn mam into data.frame for MCMCglmm
+mam <- as.data.frame(mam)
 
+# Number of species
 n.mam <- nrow(mam)
 
+# Combine datasets
 df <- rbind(mam, pantheria)
-df <- as.data.frame(df)
 
+# Reduce forest to the designated species list
 forest <- readRDS("builds/forest.rds")
 species <- forest[[1]]$tip.label
 drop.species <- species[!species %in% terrestrial]
